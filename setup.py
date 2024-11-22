4060c0e1b6be5408f28ea455e492515c36db5a8c
Leonie Dragun <82779352+dragun0@users.noreply.github.com>
2024-11-22 15:29:28 +0100

    upgraded versions of ai-models and ai-models-graphcast packages in requirements.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Allows people without massive GPUs to easily run graphcast on remote runpod servers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Lewington-pitsos/remote-graphcast",
+    url="https://github.com/dragun0/remote-graphcast-CDSapi-0.7.x",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
